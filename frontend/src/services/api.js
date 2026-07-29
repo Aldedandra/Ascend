e94b99c36df5ab1e06f5a247ca660d76906a462d
Aldedandra <bryant.blackham1@gmail.com>
2026-07-29 13:13:03 -0600

    feat: add native iOS support with Capacitor

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,7 @@
+const API_BASE_URL = "http://100.105.129.14:8000";
+
 const request = async (path, options = {}) => {
-  const response = await fetch(path, {
+  const response = await fetch(`${API_BASE_URL}${path}`, {
     headers: {
       "Content-Type": "application/json",
       ...(options.headers || {}),
